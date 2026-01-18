b306272df914a68d04a1e870e2afd8c56d25dc3c
blazej <blazejgolda123@gmail.com>
2026-01-18 17:05:21 +0100

    aktualizacja formularzy dodanie zawartosci

--- skrypt.js
+++ skrypt.js
@@ -1,1 +1,17 @@
 const ktoraOpcja=document.getElementById("dalej");
+const ekran=document.getElementById("formularz");
+const ekran_dodaj_trening=document.getElementById("dodaj_trening");
+ktoraOpcja.addEventListener("click", function(){
+    const zmienna=document.querySelector('input[name="opcja"]:checked');
+    if (!zmienna){
+        alert("Wybierz opcje!");
+        return;
+    }
+    else{
+        const option=zmienna.value;
+        if (option=="trening"){
+            ekran_dodaj_trening.style.display="block";
+            ekran.style.display="none";
+        }
+    }
+});
